d5a1390e1502723c7566d878da1ceee053b8317c
Quinn Stiegman <quinn.stiegman@gmail.com>
2025-01-29 20:45:49 -0600

    rewroking scraping code, adding csv

--- scrape-alaskaair.py
+++ scrape-alaskaair.py
@@ -11,6 +11,9 @@ import pandas as pd
 import os
 from seleniumbase import Driver
 
+future_date = datetime.now() + timedelta(days=365)
+formatted_date = future_date.strftime('%Y-%m-%d')
+print(formatted_date)
 # Set up WebDriver (replace with the path to your driver executable)
 driver = Driver(uc=True,headless=True, disable_gpu=True)
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
@@ -22,43 +25,56 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 })
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'})
 
+# Set up the CSV file
+csv_file_path = 'scrape_results.csv'
+csv_headers = ['Date', 'Route','Price', 'Timestamp']
+
 try:
-    print('PPT --> LAX')
-    print('-------------------------------')
-    # Define the target URL, gift card number, and list of PIN numbers
-    url = "https://www.alaskaair.com/search/results?A=1&O=PPT&D=LA5&OD=2025-10-18&OT=Anytime&RT=false&UPG=none&ShoppingMethod=onlineaward&awardType=MilesOnly"  # Replace with the actual URL
+    with open(csv_file_path, mode='a', newline='') as file:
+        csv_writer = csv.writer(file)
+        if os.stat(csv_file_path).st_size == 0:
+            csv_writer.writerow(csv_headers)
+        print('PPT --> LAX')
+        route = 'PPT --> LAX'
+        print('-------------------------------')
+        # Define the target URL, gift card number, and list of PIN numbers
+        url = f"https://www.alaskaair.com/search/results?A=1&O=PPT&D=LA5&OD={formatted_date}&OT=Anytime&RT=false&UPG=none&ShoppingMethod=onlineaward&awardType=MilesOnly"  # Replace with the actual URL
 
-    # Navigate to the original page for each PIN attempt
-    driver.get(url)
+        # Navigate to the original page for each PIN attempt
+        driver.get(url)
 
-    # Wait for the element to be present
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='valuetile-0-2']"))
-    )
-    print(element.text)
+        # Wait for the element to be present
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='valuetile-0-2']"))
+        )
+        csv_writer.writerow([formatted_date, route, element.text, datetime.now()])
+        print(element.text)
 
 except:
     print('Day not availiable yet')
 
 try:
-    print('')
-    print('LAX --> PPT')
-    print('-------------------------------')
-    # Define the target URL, gift card number, and list of PIN numbers
-    url = "https://www.alaskaair.com/search/results?A=1&O=LA5&D=PPT&OD=2025-10-18&OT=Anytime&RT=false&UPG=none&ShoppingMethod=onlineaward&awardType=MilesOnly"  # Replace with the actual URL
+    with open(csv_file_path, mode='a', newline='') as file:
+        csv_writer = csv.writer(file)
+        if os.stat(csv_file_path).st_size == 0:
+            csv_writer.writerow(csv_headers)
+        print('')
+        print('LAX --> PPT')
+        route = 'LAX --> PPT'
+        print('-------------------------------')
+        # Define the target URL, gift card number, and list of PIN numbers
+        url = f"https://www.alaskaair.com/search/results?A=1&O=LA5&D=PPT&OD={formatted_date}&OT=Anytime&RT=false&UPG=none&ShoppingMethod=onlineaward&awardType=MilesOnly"  # Replace with the actual URL
 
-    # Navigate to the original page for each PIN attempt
-    driver.get(url)
+        # Navigate to the original page for each PIN attempt
+        driver.get(url)
 
-    # Wait for the element to be present
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='valuetile-0-2']"))
-    )
-    print(element.text)
-
+        # Wait for the element to be present
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='valuetile-0-2']"))
+        )
+        csv_writer.writerow([formatted_date, route, element.text, datetime.now()])
+        print(element.text)
 except:
     print('Day not availiable yet')
 
-finally:
-    # Close the WebDriver
-    driver.quit()
+
